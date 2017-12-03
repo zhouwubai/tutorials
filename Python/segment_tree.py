f867e15066f603017d58c31ee799a665072aa742
Wubai Zhou <wubai.zhou@uber.com>
2017-12-03 14:28:51 -0800

    add segment tree

---
+++ Python/segment_tree.py
@@ -0,0 +1,27 @@
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+
+class SEGNode(object):
+    def __init__(self):
+        self.sum = None
+        self.left = None
+        self.right = None
+
+
+def makeSegTree(arr):
+    pass
+
+
+def getSum(root, start, end):
+    pass
+
+
+def update(root, idx, new_val):
+    pass
+
+
+if __name__ == '__main__':
+    pass
